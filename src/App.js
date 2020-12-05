0843ec30df1d77b40cf8d7bca175cd7b0da85c7d
nikita <prokayev@icloud.com>
2020-12-05 15:19:38 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>By there</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I was changed on feature branch</p>
       </header>
     </div>
   );
